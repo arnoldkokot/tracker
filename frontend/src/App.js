9111c922f98b9ae7112fe1b03841bcb468ad836a
Arnold Kokot <ogkokot@gmail.com>
2022-04-02 15:12:28 +0200

    primer attached

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,16 @@
+import React from "react";
+import { Button } from "@primer/react";
+import { ThemeProvider, BaseStyles } from "@primer/react";
+
 export default function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <ThemeProvider colorMode="day">
+      <BaseStyles>
+        <div className="App">
+          Hello world
+          <Button>Default</Button>
+        </div>
+      </BaseStyles>
+    </ThemeProvider>
+  );
 }
